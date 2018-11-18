2158bc2ef03a29789854cf12c0858a6cda437b7d
StephSako <stephen.sakovitch@orange.fr>
2018-11-18 18:21:10 +0100

    Archi MVC

---
+++ development/model/Utilisateur.php
@@ -0,0 +1,11 @@
+<?php
+	class Utilisateur {
+	    public $nom = $POST['nom'];
+	    public $prenom = $POST['prenom'];
+	   
+	    function creerUtilisateur(nom, prenom) {
+	        this.nom = nom;
+	        this.prenom = prenom;
+	    }
+	}
+?> 
